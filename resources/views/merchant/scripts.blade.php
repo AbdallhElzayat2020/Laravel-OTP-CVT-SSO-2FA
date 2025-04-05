9ef055098bc2b5682803456b6e74add59a8cf3c2
Abdallh ELzayat <121009680+AbdallhElzayat2020@users.noreply.github.com>
2025-04-05 07:32:08 +0200

    add merchant layout files including navbar, sidebar, footer, head, and scripts

---
+++ resources/views/merchant/scripts.blade.php
@@ -0,0 +1,16 @@
+<script src="{{asset('merchant-assets')}}/vendor/libs/jquery/jquery.js"></script>
+<script src="{{asset('merchant-assets')}}/vendor/libs/popper/popper.js"></script>
+<script src="{{asset('merchant-assets')}}/vendor/js/bootstrap.js"></script>
+<script src="{{asset('merchant-assets')}}/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+<script src="{{asset('merchant-assets')}}/vendor/js/menu.js"></script>
+
+<!-- endbuild -->
+
+<!-- Vendors JS -->
+<script src="{{asset('merchant-assets')}}/vendor/libs/apex-charts/apexcharts.js"></script>
+
+<!-- Main JS -->
+<script src="{{asset('merchant-assets')}}/js/main.js"></script>
+
+<!-- Page JS -->
+<script src="{{asset('merchant-assets')}}/js/dashboards-analytics.js"></script>
