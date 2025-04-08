5a80cb9b998d486b6c00533d2a74f006a2e91161
Abdallh ELzayat <121009680+AbdallhElzayat2020@users.noreply.github.com>
2025-04-08 09:19:34 +0200

    implement custom email verification notification for merchants and update verification logic

--- app/Http/Controllers/MerchantAuth/VerifyEmailController.php
+++ app/Http/Controllers/MerchantAuth/VerifyEmailController.php
@@ -14,12 +14,12 @@ class VerifyEmailController extends Controller
      */
     public function __invoke(EmailVerificationRequest $request): RedirectResponse
     {
-        if ($request->user()->hasVerifiedEmail()) {
+        if ($request->user('merchant')->hasVerifiedEmail()) {
             return redirect()->intended(route('merchant.index', absolute: false).'?verified=1');
         }
 
         if ($request->user()->markEmailAsVerified()) {
-            event(new Verified($request->user()));
+            event(new Verified($request->user('merchant')));
         }
 
         return redirect()->intended(route('merchant.index', absolute: false).'?verified=1');
--- app/Models/Merchant.php
+++ app/Models/Merchant.php
@@ -3,15 +3,34 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Notifications\MerchantEmailVerification;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\URL;
 
 class Merchant extends Authenticatable implements MustVerifyEmail
 {
+
+
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use Notifiable;
+
+
+    // custom Notification for Merchant
+    public function sendEmailVerificationNotification()
+    {
+        $url = URL::temporarySignedRoute(
+            'merchant.verification.verify', now()->addMinutes(30),
+            [
+                'id' => $this->getKey(), //  $this->>id
+                'hash' => sha1($this->getEmailForVerification()), // $this->email
+            ]
+        );
+
+        $this->notify(new MerchantEmailVerification($url));
+    }
 
     /**
      * The attributes that are mass assignable.
